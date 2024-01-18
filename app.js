9f9bc5784434c76681aabe815c2620f462426414
partelsandra <sandra.partel.001@voco.ee>
2024-01-18 16:36:06 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const todoRoutes = require("./routes/todo.routes");
+const app = express()
+
+app.use(express.json())
+
+app.use("/todos", todoRoutes);
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+
+app.listen(3015, () => {
+    console.log('server is running')
+})
+
+module.exports = app
---
+++ controllers/todo.controller.js
@@ -0,0 +1,8 @@
+const TodoModel = require('../models/todo.model')
+
+const createTodo = (req, res, next) => {
+    const createModel = TodoModel.create(req.body)
+    res.status(201).json(createdModel)
+}
+
+module.exports = {createTodo}
