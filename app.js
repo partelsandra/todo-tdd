7e4f2d915a9322a0e2b7da8c8ecfbf1c44866c28
partelsandra <sandra.partel.001@voco.ee>
2024-01-18 17:00:54 +0200

    commit

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ app.get('/', function (req, res) {
     res.send('Hello World')
 })
 
-app.listen(3015, () => {
-    console.log('server is running')
-})
+//app.listen(3015, () => {
+    //console.log('server is running')
+//})
 
 module.exports = app
