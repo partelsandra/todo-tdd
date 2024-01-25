f96b6a01ae8a07cfd65919a4d074f9bfd8a9242b
partelsandra <sandra.partel.001@voco.ee>
2024-01-25 14:20:56 +0200

    Created TodoController.getTodos unit test and integration tests

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,12 +1,24 @@
-const TodoModel = require('../models/todo.model')
+const TodoModel = require('../models/todo.model');
 
 const createTodo = async (req, res, next) => {
     try {
-    const createModel = await TodoModel.create(req.body)
-    res.status(201).json(createdModel)
-    } catch(error) {
-        next(error)
+        const createdModel = await TodoModel.create(req.body);
+        res.status(201).json(createdModel);
+    } catch (error) {
+        next(error);
     }
-}
+};
 
-module.exports = {createTodo}
+const getTodos = async (req, res, next) => {
+    try { 
+        const allTodos = await TodoModel.find({});
+        res.status(200).json(allTodos);
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports = {
+    createTodo,
+    getTodos
+};
--- tests/integration/todo.controller.int.test.js
+++ tests/integration/todo.controller.int.test.js
@@ -26,4 +26,12 @@ describe(endpointUrl, () => {
             });
         }
     );
+
+    it("GET " + endpointUrl, async () => { 
+        const response = await request(app).get(endpointUrl);
+        expect(response.statusCode).toBe(200);
+        expect(Array.isArray(response.body)).toBeTruthy();
+        expect(response.body[0].title).toBeDefined();
+        expect(response.body[0].done).toBeDefined();
+    });
 });
