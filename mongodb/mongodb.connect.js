6b43d898f0e44af68d3ee37fb50aa9cb5728e403
partelsandra <sandra.partel.001@voco.ee>
2024-01-25 13:26:53 +0200

    mongodb

---
+++ mongodb/mongodb.connect.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+async function connect() {
+    try {
+        await mongoose.connect(
+            "mongodb+srv://root:<qwerty12345>@cluster0.6odvfgc.mongodb.net/",
+            {
+                useNewUrlParser: true,
+            }
+        );
+    } catch (err) {
+        console.error("Error connecting to MongoDB");
+    }
+}
+
+module.exports = { connect };
