3bc55969be8d810aae7b08d5195e65a215eb05bb
partelsandra <sandra.partel.001@voco.ee>
2024-01-25 13:53:15 +0200

    “Create error handling for unit and integration tests”

--- app.js
+++ app.js
@@ -7,10 +7,17 @@ mongodb.connect();
 
 app.use(express.json())
 
+app.use((error, rew, res, next) => {
+    //console.log(error)
+    res.status(500).json({ message: error.message});
+});
+
 app.use("/todos", todoRoutes);
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
+
+app.get('/', (req, res) => {
+    res.send('express test');
+});
+
 
 //app.listen(3015, () => {
     //console.log('server is running')
--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,8 +1,12 @@
 const TodoModel = require('../models/todo.model')
 
 const createTodo = async (req, res, next) => {
+    try {
     const createModel = await TodoModel.create(req.body)
     res.status(201).json(createdModel)
+    } catch(error) {
+        next(error)
+    }
 }
 
 module.exports = {createTodo}
